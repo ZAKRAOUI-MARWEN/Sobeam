8bfc9f0be37b8732fa768c30b22502f6579a7afd
ZAKRAOUI-MARWEN <marwenzakraoui03@gmail.com>
2024-10-14 08:35:00 +0100

    edit licence and traduction

--- ui-ngx/src/app/app.component.ts
+++ ui-ngx/src/app/app.component.ts
@@ -1,5 +1,5 @@
 ///
-/// Copyright © 2016-2024 The Thingsboard Authors
+/// Copyright © 2016-2024 The Sobeam Authors
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
@@ -48,7 +48,7 @@ export class AppComponent implements OnInit {
               private domSanitizer: DomSanitizer,
               private authService: AuthService) {
 
-    console.log(`ThingsBoard Version: ${env.tbVersion}`);
+    console.log(`Sobeam Version: ${env.tbVersion}`);
 
     this.matIconRegistry.addSvgIconResolver((name, namespace) => {
       if (namespace === 'mdi') {
